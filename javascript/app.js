1e359e62379c76c549b0ba52c24b71e14466f8f2
thaerbraizat <thaerbraizat13@gmail.com>
2021-04-04 15:04:42 +0300

    add new repo

---
+++ javascript/app.js
@@ -0,0 +1,10 @@
+let favHero = prompt("what's your favorite hero?")
+console.log(favHero);
+let posPlay = prompt("what's your postion?")
+console.log(posPlay);
+let newRank = prompt("what's your rank ?")
+console.log(newRank);
+let timePlay = prompt("for how long have you been playing dota?")
+console.log(timePlay);
+
+alert('your favorite hero is  '+favHero +' and your postion is '+posPlay +' and your rank is '+newRank +' and you have been playing dota for '+timePlay )
